39c5ca67dd3896870ddcf63e753b0aede306b813
ThaiNgocHuuBang <thaingochuubang10@gmail.com>
2025-10-02 23:38:05 +0700

    video 31

--- src/component/learn/MyComponent.jsx
+++ src/component/learn/MyComponent.jsx
@@ -1,9 +1,18 @@
 import "./style.css";
 const MyComponent = () => {
+  //   const myText = "Hello Bruyden";
+
+  //   const myText = 1;
+  //   const myText = Boolean;
+  //   const myText = undefined;
+  //   const myText = null;
+  const myText = [1, 2, 3];
   return (
     <>
       <div style={{ color: "green" }}>MyComponent</div>
-      <div className="bruyden">Bruyden</div>
+      <div className="bruyden">{myText}</div>
+      <div>{JSON.stringify(myText)}</div>
+      <div>{console.log("hello")}</div>
     </>
   );
 };
