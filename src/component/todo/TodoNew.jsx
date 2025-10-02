f3b451702e8304ebe8626d69b90891b13dadb660
ThaiNgocHuuBang <thaingochuubang10@gmail.com>
2025-10-03 03:25:06 +0700

    video 36

--- src/component/todo/TodoNew.jsx
+++ src/component/todo/TodoNew.jsx
@@ -1,11 +1,26 @@
 const TodoNew = (prpos) => {
   console.log(prpos);
   const { addNewTodo } = prpos;
-  addNewTodo(`bang`);
+  // addNewTodo(`bang`);
+  const handleClick = () => {
+    alert("click me");
+  };
+  // const handleChange = (event) => {
+  //   console.log("onchange", event.target.value);
+  // };
+  const handleChange = (name) => {
+    console.log("onchange", name);
+  };
   return (
     <div className="todo-new">
-      <input placeholder="Enter your task" type="text" />
-      <button>ADD</button>
+      <input
+        onChange={(event) => {
+          handleChange(event.target.value);
+        }}
+        placeholder="Enter your task"
+        type="text"
+      />
+      <button onClick={handleClick}>ADD</button>
     </div>
   );
 };
