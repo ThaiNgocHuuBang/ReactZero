3f1d75fb85d21317105c9746cb813f72e8b3cd6b
ThaiNgocHuuBang <thaingochuubang10@gmail.com>
2025-10-17 02:39:16 +0700

    video67

--- src/component/user/user.form.jsx
+++ src/component/user/user.form.jsx
@@ -1,25 +1,56 @@
 import { Input, Button } from "antd";
+import { useState } from "react";
 const UseForm = () => {
+  const [fullName, setFullName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [phone, setPhone] = useState();
+  const handleCick = () => {
+    alert("Clicked");
+    console.log("clicked", { fullName, email, password, phone });
+  };
   return (
     <div>
       <div style={{ margin: 20 }}>
         <div>
           <span>Full Name</span>
-          <Input />
+          <Input
+            value={fullName}
+            onChange={(event) => {
+              setFullName(event.target.value);
+            }}
+          />
         </div>
         <div>
           <span>Email</span>
-          <Input />
+          <Input
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
         </div>
         <div>
           <span>Password</span>
-          <Input.Password />
+          <Input.Password
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
         </div>
         <div>
           <span>Phone number</span>
-          <Input />
+          <Input
+            value={phone}
+            onChange={(event) => {
+              setPhone(event.target.value);
+            }}
+          />
         </div>
-        <Button type="primary">Create User</Button>
+        <Button type="primary" onClick={handleCick}>
+          Create User
+        </Button>
       </div>
     </div>
   );
