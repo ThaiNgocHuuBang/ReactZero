fbd595d0f2c79cb0db4bb611a5c2bbf5cf88e485
ThaiNgocHuuBang <thaingochuubang10@gmail.com>
2025-10-16 23:32:09 +0700

    video52

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,15 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/login.jsx";
 import RegisterPage from "./pages/register.jsx";
 import UsersPage from "./pages/user.jsx";
-import ProductsPage from "./pages/product.jsx";
+import BookPage from "./pages/books.jsx";
 import "../src/styles/global.css";
 import TodoApp from "./component/todo/TodoApp.jsx";
+import ErrorPage from "./pages/error.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
@@ -22,8 +24,8 @@ const router = createBrowserRouter([
         element: <UsersPage />,
       },
       {
-        path: "/products",
-        element: <ProductsPage />,
+        path: "/books",
+        element: <BookPage />,
       },
     ],
   },
